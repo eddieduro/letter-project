88d051e48b79a5ff67afc21bd2db1148d74a6c26
eddieduro <student@epicodus.com>
2016-01-12 10:44:59 -0800

    add js functionality when form is filled and submitted

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$("document").ready(function() {
+  $("#blank form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipcodeInput = $("input#zipcode").val();
+
+    $(".name").text(nameInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zipcode").text(zipcodeInput);
+
+    $("#address").toggle();
+    event.preventDefault();
+  });
+});
